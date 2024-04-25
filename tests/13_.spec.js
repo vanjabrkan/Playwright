76fe386cb5554d97697f1a0ab84fcfb27c1c19c6
Vanja <vanjabrkans@gmail.com>
2024-04-25 20:53:25 +0200

    How to handle Bootstrap multi select DropDown

--- tests/13_.spec.js
+++ tests/13_.spec.js
@@ -1,13 +1,44 @@
 const {test, expect} = require('@playwright/test')
 
-test('Handle Dropdown', async ({page})=>{
+test('Bootstrap Dropdown', async ({page})=>{
 
-    await page.goto('https://testautomationpractice.blogspot.com/');
+    await page.goto('https://www.jquery-az.com/boots/demo.php?ex=63.0_2');
 
+    await page.locator('.multiselect').click(); //click on the dropdown
 
+    //1)
+    //const options= await page.locator('ul>li label input')
+    // await expect(options).toHaveCount(11)
 
+    //2)
+    //const options= await page.$$('ul>li label input')
+    //await expect(options.length).toBe(11)
 
+    //3) select options from dropdown
+/*    const options= await page.$$('ul>li label')
 
+    for(let option of options)
+    {
+        const value = await option.textContent();
+        //console.log("Value is:", value);
+        if(value.includes('Angular') || value.includes('Java'))
+        {
+            await option.click()
+        }
+    }
+*/
+    //3) deselect options from dropdown
+    const options= await page.$$('ul>li label')
+
+    for(let option of options)
+    {
+        const value = await option.textContent();
+        //console.log("Value is:", value);
+        if(value.includes('HTML') || value.includes('CSS'))
+        {
+            await option.click()
+        }
+    }
     await page.waitForTimeout(5000); //pausing code
     
 })
